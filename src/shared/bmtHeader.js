6453d48b013185c366ed841dd7866a5a6851f414
3dprintedcat <8skittles5@gmail.com>
2023-02-25 00:31:05 -0500

    bug fix

--- src/shared/bmtHeader.js
+++ src/shared/bmtHeader.js
@@ -7,7 +7,7 @@ const BMTHeader = () =>{
     let screenSize = useBreakpoint();
     const { token } = useToken();
     return(
-    <header className="App-header" style={screenSize.xs ? {backgroundColor:token.colorPrimary, height:"7vh", position:"sticky", top:0,}:{backgroundColor:token.colorPrimary, height:"7vh", position:"sticky", top:0,}}>
+    <header className="App-header" style={screenSize.xs ? {backgroundColor:token.colorPrimary, height:"7vh", position:"sticky", top:0,zIndex:"2"}:{backgroundColor:token.colorPrimary, height:"7vh", position:"sticky", top:0,zIndex:"2"}}>
         <img src="/logo192.png" style={screenSize.xs ? {height:"6vh",float:"left", marginLeft:"1%",marginTop:"1%"} : {height:"5vh",float:"left", marginTop:"1vh", marginLeft:"0.5em"}}></img>
       </header>);
 };
